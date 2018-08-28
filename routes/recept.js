91dcda3032591db4585da6c93d327c60e809efcc
Jevgeni van Steenis <jevgenivansteenis@gmail.com>
2018-08-28 15:22:31 +0200

    Configured restfull api

--- routes/recept.js
+++ routes/recept.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Recept = require('../models/Recept.js');
 
-// Getting the homepage
+// Get all recepies
 router.get('/', function(req, res, next) {
-  res.send('Express REST API');
+  Recept.find(function(err, recepten) {
+    if (err) return next(err);
+    res.json(recepten);
+  });
+});
+
+// Get recepie by id
+router.get('/', function(req, res, next) {
+  Recept.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+// Save recepie
+router.post('/', function(req, res, next) {
+  Recept.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+// Update recepie
+router.post('/:id', function(req, res, next) {
+  Recept.findByIdAndUpdate(req.params.id, req.body, function(err, post) {
+    if (err) return next(err);
+    res.json(post);
+  })
+});
+
+// Delete recepie
+router.delete('/:id', function(req, res, next) {
+  Recept.findByIdAndRemove(req.params.id, req.body, function(err, post) {
+    if (err) return next(err);
+    res.json(post);
+  })
 });
 
 module.exports = router;
